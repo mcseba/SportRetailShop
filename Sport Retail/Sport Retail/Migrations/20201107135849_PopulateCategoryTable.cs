600d0fcefeb0f519d66e7050120a23e77e0e0acc
mcseba <48526736+mcseba@users.noreply.github.com>
2020-11-07 15:13:59 +0100

    Category table populated using migration

---
+++ Sport Retail/Sport Retail/Migrations/20201107135849_PopulateCategoryTable.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Sport_Retail.Migrations
+{
+    public partial class PopulateCategoryTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Category (Name) VALUES ('Sporty wodne')");
+            migrationBuilder.Sql("INSERT INTO Category (Name) VALUES ('Piłka nożna')");
+            migrationBuilder.Sql("INSERT INTO Category (Name) VALUES ('Akcesoria')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
