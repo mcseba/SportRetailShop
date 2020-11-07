148648fbe5fc3a66091b117b1018f021dbe050ac
mcseba <48526736+mcseba@users.noreply.github.com>
2020-11-07 13:55:47 +0100

    Database repository

---
+++ Sport Retail/Sport Retail/Models/EFProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sport_Retail.Models
+{
+    public class EFProductRepository : IProductRepository
+    {
+        private readonly AppDbContext _ctx;
+
+        public EFProductRepository(AppDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public IQueryable<Product> Products => _ctx.Products;
+    }
+}
