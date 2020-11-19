9bfe7531b6ad24f4dbd97e98bf009727c8df8e6c
mcseba <48526736+mcseba@users.noreply.github.com>
2020-11-19 13:47:13 +0100

    New controller for admin user, managing products in database

---
+++ Sport Retail/Sport Retail/Controllers/AdminController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sport_Retail.Models;
+
+namespace Sport_Retail.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly IProductRepository _repository;
+        public AdminController(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+        public IActionResult Index()
+        {
+            var products = _repository.Products.Include(p => p.Category);
+            return View(products);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var productToEdit = _repository.Products.Single(p => p.Id == id);
+
+            return View(productToEdit);
+        }
+
+        public IActionResult Save(Product product)
+        {
+            _repository.SaveProduct(product);
+            TempData["SuccessSave"] = "Zapisano produkty w bazie danych";
+
+            return RedirectToAction("Index", "Admin");
+        }
+
+        public IActionResult Create()
+        {
+            return View("Edit", new Product());
+        }
+
+        public IActionResult Delete(int id)
+        {
+            _repository.DeleteProduct(id);
+            TempData["SuccessDelete"] = "Usunieto produkt z bazy danych";
+
+            return RedirectToAction("Index", "Admin");
+        }
+    }
+}
