2c59921f2372bb77c2951978549321a6051860c0
mcseba <48526736+mcseba@users.noreply.github.com>
2020-11-20 16:13:14 +0100

    View Model for presenting and manipulating data

---
+++ Sport Retail/Sport Retail/ViewModels/ProductFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Sport_Retail.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sport_Retail.ViewModels
+{
+    public class ProductFormViewModel
+    {
+        public IEnumerable<Category> Categories { get; set; }
+        public Product Product { get; set; }
+    }
+}
