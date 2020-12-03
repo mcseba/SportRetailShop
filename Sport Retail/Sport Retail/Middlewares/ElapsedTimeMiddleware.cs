cadc133a07b801f59a6d205625cba2d76eec91a3
mcseba <48526736+mcseba@users.noreply.github.com>
2020-12-03 15:18:13 +0100

    Middleware for counting execution time

---
+++ Sport Retail/Sport Retail/Middlewares/ElapsedTimeMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sport_Retail.Middlewares
+{
+    public class ElapsedTimeMiddleware
+    {
+        private readonly ILogger _logger;
+        private readonly RequestDelegate _next;
+
+        public ElapsedTimeMiddleware(RequestDelegate next, ILogger<ElapsedTimeMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            await _next(context);
+
+            _logger.LogInformation($"Executed in {sw.ElapsedMilliseconds}ms");
+            Console.WriteLine($"Executed in {sw.ElapsedMilliseconds}ms");
+        }
+    }
+}
