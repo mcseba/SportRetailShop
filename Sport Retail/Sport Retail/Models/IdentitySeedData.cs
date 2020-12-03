60ef289cf28b846f842a8c30cd4378e79ebb13fd
mcseba <48526736+mcseba@users.noreply.github.com>
2020-12-03 15:16:01 +0100

    Helping class for creating account

---
+++ Sport Retail/Sport Retail/Models/IdentitySeedData.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sport_Retail.Models
+{
+    public static class IdentitySeedData
+    {
+        private const string adminUser = "Admin";
+        private const string adminPassword = "admin123";
+
+        public static async Task EnsurePopulated(UserManager<IdentityUser> userManager)
+        {
+
+            IdentityUser user = await userManager.FindByIdAsync(adminUser);
+            if (user == null)
+            {
+                user = new IdentityUser("Admin");
+                await userManager.CreateAsync(user, adminPassword);
+            }
+        }
+    }
+}
