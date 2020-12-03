5221c73b9ea5e9f6b8ba110b15821bdf44e8b23f
mcseba <48526736+mcseba@users.noreply.github.com>
2020-12-03 15:13:42 +0100

    Added controller for login in admin account

---
+++ Sport Retail/Sport Retail/Controllers/AccountController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Sport_Retail.Models;
+using Sport_Retail.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sport_Retail.Controllers
+{
+    public class AccountController: Controller
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
+
+        public AccountController(UserManager<IdentityUser> userMgr,
+                SignInManager<IdentityUser> signInMgr)
+        {
+            userManager = userMgr;
+            signInManager = signInMgr;
+
+            IdentitySeedData.EnsurePopulated(userMgr).Wait();
+        }
+
+        [AllowAnonymous]
+        public ViewResult Login(string returnUrl)
+        {
+            return View(new LoginViewModel
+            {
+                ReturnUrl = returnUrl
+            });
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel loginModel)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityUser user = await userManager.FindByNameAsync(loginModel.Name);
+
+                if (user != null)
+                {
+                    await signInManager.SignOutAsync();
+                    if((await signInManager.PasswordSignInAsync(user, 
+                        loginModel.Password, false, false)).Succeeded)
+                    {
+                        return Redirect(loginModel?.ReturnUrl ?? "/Admin/Index");
+                    }
+                }
+            }
+            ModelState.AddModelError("", "Nieprawidłowa nazwa użytkownika lub hasło");
+            return View(loginModel);
+        }
+
+        public async Task<RedirectResult> Logout(string returnUrl = "/")
+        {
+            await signInManager.SignOutAsync();
+            return Redirect(returnUrl);
+        }
+    }
+}
