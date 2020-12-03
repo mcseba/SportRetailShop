0c8c37f965b7b7b4b3d6df3463c000567d7779dc
mcseba <48526736+mcseba@users.noreply.github.com>
2020-12-03 15:19:10 +0100

    changed DbContext to IdentityDbContext

--- Sport Retail/Sport Retail/Models/AppDbContext.cs
+++ Sport Retail/Sport Retail/Models/AppDbContext.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Sport_Retail.Models
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext<IdentityUser>
     {
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
